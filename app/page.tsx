487faca921e80f2f788adcca38c14126cf7607fe
elmira1385 <elmirashokriyan64@gmail.com>
2026-08-19 18:33:17 +0330

    start hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>Hi</div>
+    <div className="flex flex-col gap-4 justify-center items-center">
+      <Hero/>
+    </div>
   );
 }
