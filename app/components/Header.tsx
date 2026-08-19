057261eb2dc227037a3ff249a0d51500d26c1228
elmira1385 <elmirashokriyan64@gmail.com>
2026-08-19 18:18:12 +0330

    done header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Activity, useState } from "react";
+import { Activity, useState } from "react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -15,7 +15,7 @@ const Header = () => {
           }}
         >
           {isMenuOpen ? (
-             <svg
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -29,9 +29,8 @@ const Header = () => {
               <path d="M18 6 6 18"></path>
               <path d="m6 6 12 12"></path>
             </svg>
-          
           ) : (
-             <svg
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -50,10 +49,61 @@ const Header = () => {
         </button>
       </div>
       <Activity mode={isMenuOpen ? "visible" : "hidden"}>
-        <div className="absolute w-full right-0 top-14 flex flex-col gap-4 bg-red-100 p-4">
-          <p>hi</p>
-          <p>hi</p>
-          <p>hi</p>
+        <div className="absolute w-full right-0 top-16 flex flex-col gap-2 bg-white p-4">
+          <div className="flex gap-2 p-2  items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
+              <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            </svg>
+            <p className="text-sm font-semibold">Home</p>
+          </div>
+          <div className="flex gap-2 p-2  items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z"></path>
+              <path d="M12 22V12"></path>
+              <polyline points="3.29 7 12 12 20.71 7"></polyline>
+              <path d="m7.5 4.27 9 5.15"></path>
+            </svg>
+            <p className="text-sm font-semibold">Products</p>
+          </div>
+          <div className="flex gap-2 p-2 bg-textHeader items-center rounded-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+              <polyline points="10 17 15 12 10 7"></polyline>
+              <line x1="15" x2="3" y1="12" y2="12"></line>
+            </svg>
+            <p className="text-sm font-semibold">Sign Up</p>
+          </div>
         </div>
       </Activity>
     </header>
